3043abfbcea45b5f2c8a2d061a063e4af2bbf169
Mr HACHIMI Atmane <147668592+HACHIMIATMANE@users.noreply.github.com>
2024-07-15 18:34:19 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed('.text', {
+    strings: ["Data Engineer", "Data Scientist", "Software Developper",'AI/ML Specialist','Machine Learning Engineer' ,"Designer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
